c16d218da73f0aed37402a104e5cf024aa402c9c
wang-howard <105531941+wang-howard@users.noreply.github.com>
2024-01-10 14:50:30 -0500

    first draft

--- scraper.py
+++ scraper.py
@@ -3,11 +3,11 @@ from listennotes import podcast_api
 
 # If api_key is None, the sdk will connect to a mock server that'll
 # return fake data for testing purpose            
-api_key = "a3fb06b09a654cf28f6e9013e3206a30"
+api_key = "c63022e5c4214af6b8794a5250e2b73d"
 client = podcast_api.Client(api_key=api_key)      
 
-def scrape(term: str):
-  response = client.batch_fetch_episodes(
+def get_episodes(term: str):
+  response = client.search(
     q=term,
     sort_by_date=1,
     type="episode",
@@ -17,8 +17,13 @@ def scrape(term: str):
 
 def main():
   for line in fileinput.input(encoding="utf-8"):
-    print(scrape(line))
-    break
+    data = get_episodes(line)
+    for episode in data["results"]:
+      podcast_name = episode["podcast"]["title_original"]
+      episode_name = episode["title_original"]
+      link = episode["listennotes_url"]
+      print(f"{podcast_name} (podcast): \"{episode_name}\" ({link})")
+      print()
 
 if __name__ == "__main__":
   main()
