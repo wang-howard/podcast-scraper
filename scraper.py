5cf1869c73674372fed4ace51630ef7ae2ce113d
wang-howard <105531941+wang-howard@users.noreply.github.com>
2024-01-10 14:57:54 -0500

    add argparse

--- scraper.py
+++ scraper.py
@@ -1,10 +1,19 @@
-import fileinput
+import fileinput, argparse
 from listennotes import podcast_api
 
 # If api_key is None, the sdk will connect to a mock server that'll
 # return fake data for testing purpose            
 api_key = "c63022e5c4214af6b8794a5250e2b73d"
 client = podcast_api.Client(api_key=api_key)      
+
+def parse_args():
+  parser = argparse.ArgumentParser(
+    description="Simple podcast episode scraper for search key terms using Listen Notes API",
+    allow_abbrev=False)
+  parser.add_argument("filename",
+    help="the file containing a list of search terms")
+  parser.add_argument("--exact")
+  return parser.parse_args()
 
 def get_episodes(term: str):
   response = client.search(
@@ -16,6 +25,8 @@ def get_episodes(term: str):
   return response.json()
 
 def main():
+  parser = parse_args()
+  
   for line in fileinput.input(encoding="utf-8"):
     data = get_episodes(line)
     for episode in data["results"]:
